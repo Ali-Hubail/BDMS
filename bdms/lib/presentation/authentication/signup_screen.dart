8528461d684fc09d130ca72511380bcd844cd4f9
Rush <haidar.abugurain@gmail.com>
2023-12-15 16:54:30 +0300

    modify sign up page

--- bdms/lib/presentation/authentication/signup_screen.dart
+++ bdms/lib/presentation/authentication/signup_screen.dart
@@ -3,98 +3,111 @@ import 'package:bdms/common_widgets/primary_input.dart';
 import 'package:bdms/domain/blood_group_enum.dart';
 import 'package:flutter/material.dart';
 
-class SignUpScreen extends StatelessWidget {
-  SignUpScreen({super.key});
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
 
-  final List<String> bloodTypes = [
-    'A+',
-    'A-',
-    'B+',
-    'B-',
-    'AB+',
-    'AB-',
-    'O+',
-    'O-',
-  ]; // Add more blood types as needed
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final _dropDownMenuController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _dropDownMenuController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    BloodGroup? selectedBloodType;
     return Scaffold(
-      backgroundColor: Colors.white,
       body: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              child: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 64,
+              ),
+              Center(
                 child: Image.asset(
                   'assets/bdms.png',
                   fit: BoxFit.cover,
                 ),
               ),
-            ),
-            Expanded(
-              flex: 2,
-              child: GridView.count(
-                crossAxisCount: 2,
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
-                childAspectRatio: (1 / .25),
-                children: const [
-                  PrimaryInput(hintText: 'Name'),
-                  PrimaryInput(hintText: 'Email'),
-                  PrimaryInput(hintText: 'Password'),
-                  PrimaryInput(hintText: 'Address'),
-                  PrimaryInput(hintText: 'Age'),
-                  PrimaryInput(hintText: 'Contact Number'),
-                ],
-              ),
-            ),
-            DropdownButton<BloodGroup>(
-                hint: const Text('Select Blood Type'),
-                value: selectedBloodType,
-                onChanged: (BloodGroup? newValue) {
-                  selectedBloodType = newValue;
-                },
-                items: [
-                  for (var bloodgroup in BloodGroup.values)
-                    DropdownMenuItem<BloodGroup>(
-                      value: bloodgroup,
-                      child: Text(bloodgroup.toString()),
+              SizedBox(
+                height: 300,
+                child: GridView(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 16,
+                    mainAxisSpacing: 16,
+                    childAspectRatio: (1 / .25),
+                  ),
+                  children: [
+                    const PrimaryInput(hintText: 'Name'),
+                    const PrimaryInput(hintText: 'Email'),
+                    const PrimaryInput(hintText: 'Password'),
+                    const PrimaryInput(hintText: 'Address'),
+                    const PrimaryInput(hintText: 'Age'),
+                    const PrimaryInput(hintText: 'Contact Number'),
+                    const PrimaryInput(hintText: 'Weight'),
+                    DropdownMenu<BloodGroup>(
+                      controller: _dropDownMenuController,
+                      dropdownMenuEntries: [
+                        for (var bloodgroup in BloodGroup.values)
+                          DropdownMenuEntry<BloodGroup>(
+                            value: bloodgroup,
+                            label: bgMap[bloodgroup]!,
+                          ),
+                      ],
                     ),
-                ]),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 12.0),
-              child: PrimaryButton(
-                text: 'Sign Up',
-                onPressed: () {},
-                buttonWidth: double.infinity,
+                  ],
+                ),
               ),
-            ),
-            const Center(
-              child: Text(
-                'Continue as Guest',
-                style: TextStyle(color: Colors.grey),
+              const SizedBox(
+                height: 128,
               ),
-            ),
-            const SizedBox(height: 12),
-            Center(
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.pop(context);
-                },
-                child: const Text(
-                  'Login',
-                  style: TextStyle(
-                    color: Color(0xFFC62C2D),
-                    fontWeight: FontWeight.bold,
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 12.0),
+                child: PrimaryButton(
+                  text: 'Sign Up',
+                  onPressed: () {},
+                  buttonWidth: double.infinity,
+                ),
+              ),
+              const Center(
+                child: Text(
+                  'Continue as Guest',
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ),
+              const SizedBox(height: 32),
+              Center(
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: TextButton(
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        color: Color(0xFFC62C2D),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    onPressed: () {},
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
