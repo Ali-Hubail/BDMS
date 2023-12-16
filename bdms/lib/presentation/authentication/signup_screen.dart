cdea85e996605105bbe50dd4f67bc692ff9179de
Rush <haidar.abugurain@gmail.com>
2023-12-16 11:42:44 +0300

    add edit medical history screen for dr/rp

--- bdms/lib/presentation/authentication/signup_screen.dart
+++ bdms/lib/presentation/authentication/signup_screen.dart
@@ -3,7 +3,6 @@ import 'package:bdms/common_widgets/primary_input.dart';
 import 'package:bdms/data/authentication_repository.dart';
 import 'package:bdms/domain/blood_group_enum.dart';
 import 'package:bdms/presentation/authentication/login_screen.dart';
-import 'package:bdms/presentation/dr_rp_home/dr_rp_home.dart';
 import 'package:flutter/material.dart';
 
 class SignUpScreen extends StatefulWidget {
